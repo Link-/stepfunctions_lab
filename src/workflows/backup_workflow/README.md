a92c403d4639b4528c833b332fc62331b5f7fd79
Bassem Dghaidi <bassem@interop.link>
2020-06-06 17:14:25 +0200

    Add build script and update readme instructions

--- src/workflows/backup_workflow/README.md
+++ src/workflows/backup_workflow/README.md
@@ -5,27 +5,24 @@
 ## Run
 
 ```sh
-# Go to the lambda directory
-cd ./lambda
-
-# Zip the lambda function so that we push the zip file to AWS
-zip payload.zip function.py
-
-# Go to the terraform directory to apply the stack
-cd ../terraform
+# Run the build script to generate the lambda function payload
+build.sh
 
 # Dry-run
 terraform plan \
-  --var="lambda_function_payload="(pwd)"/../lambda/payload.zip" \
-  --var="sfn_state_machine_definition="(pwd)"/../state_machine_definition.json.tpl"
+  --var="lambda_function_payload="(pwd)"/lambda/payload.zip" \
+  --var="sfn_state_machine_definition="(pwd)"/state_machine_definition.json.tpl" \
+  terraform
 
 # Build the stack
 terraform apply \
-  --var="lambda_function_payload="(pwd)"/../lambda/payload.zip" \
-  --var="sfn_state_machine_definition="(pwd)"/../state_machine_definition.json.tpl"
+  --var="lambda_function_payload="(pwd)"/lambda/payload.zip" \
+  --var="sfn_state_machine_definition="(pwd)"/state_machine_definition.json.tpl" \
+  terraform
 
 # Destroy the stack
 terraform destroy \
-  --var="lambda_function_payload="(pwd)"/../lambda/payload.zip" \
-  --var="sfn_state_machine_definition="(pwd)"/../state_machine_definition.json.tpl"
+  --var="lambda_function_payload="(pwd)"/lambda/payload.zip" \
+  --var="sfn_state_machine_definition="(pwd)"/state_machine_definition.json.tpl" \
+  terraform
 ```
---
+++ src/workflows/backup_workflow/build.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+# +-------------------------------+
+# | buid.sh                       |
+# | +-----------------------------+
+# |                               |
+# | This is a simple bash script  |
+# | that will compile the needed  |
+# | artefacts (like lambda        |
+# | function payload or otherwise)|
+# | and make them ready for       |
+# | Terraform to upload to AWS.   |
+# |                               |
+# | v:0.1                         |
+# +-------------------------------+
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+LAMBDA_PAYLOAD_FILE_PATH="lambda"
+LAMBDA_PAYLOAD_FILE_NAME="function.py"
+LAMBDA_PAYLOAD_ZIP_FILE_NAME="payload.zip"
+TERRAFORM_MODULE_PATH="terraform"
+
+echo "$(date +%F_%T) \t INFO:: Checking if zip is installed"
+hash zip 2>/dev/null || { echo >&2 "$(date +%F_%T) \t INFO:: I require zip but it's not installed. Aborting."; exit 1; }
+
+echo "$(date +%F_%T) \t INFO:: Checking if lambda payload file exists"
+if [[ -e "${LAMBDA_PAYLOAD_FILE_PATH}/${LAMBDA_PAYLOAD_FILE_NAME}" ]]; then
+  echo "$(date +%F_%T) \t INFO:: ${LAMBDA_PAYLOAD_FILE_NAME} exists"
+
+  echo "$(date +%F_%T) \t INFO:: Switching directory to: ${LAMBDA_PAYLOAD_FILE_PATH}"
+  cd $LAMBDA_PAYLOAD_FILE_PATH
+
+  echo "$(date +%F_%T) \t INFO:: Creating payload zip file"
+  zip -r $LAMBDA_PAYLOAD_ZIP_FILE_NAME ./$LAMBDA_PAYLOAD_FILE_NAME
+
+  # Double check if the zip command was successful and that the zip
+  # file exists
+  if [[ $? -eq 0 ]]; then
+    if [[ -e "${LAMBDA_PAYLOAD_ZIP_FILE_NAME}" ]]; then
+      echo "$(date +%F_%T) \t INFO:: ${LAMBDA_PAYLOAD_ZIP_FILE_NAME} was created successfully"
+    else
+      echo "$(date +%F_%T) \t ERROR:: failed to create ${LAMBDA_PAYLOAD_ZIP_FILE_NAME}. Make sure you have sufficient permissions and/or zip is installed"
+      exit 1
+    fi
+  else
+    echo "$(date +%F_%T) \t ERROR:: Make sure you have sufficient permissions and/or zip is installed"
+    exit 1
+  fi
+
+else
+  echo "$(date +%F_%T) \t ERROR:: ${LAMBDA_PAYLOAD_FILE_NAME} does not exist, exiting. Please make sure you are running this script from the workflow's directory."
+  exit 1
+fi
+
+# Initialize terraform
+cd ..
+echo "$(date +%F_%T) \t INFO:: switching to parent directory $(pwd)"
+echo "$(date +%F_%T) \t INFO:: initializing terraform"
+terraform init $TERRAFORM_MODULE_PATH
